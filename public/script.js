b4d51ddd0fceac0910b8e904823c7820225b4658
unknown <bhm727@yahoo.com>
2021-10-05 11:11:34 -0400

    Repo Push for JS

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-const base url = https://api.nasa.gov/planetary/apod?api_key=hJ5RIMmDbh0gds3b68OE8jQJ2IR4WZ1Qp0DF2QlL
+const baseUrl = https://api.nasa.gov/planetary/apod?api_key=hJ5RIMmDbh0gds3b68OE8jQJ2IR4WZ1Qp0DF2QlL
+
+const randNum = Math.floor(Math.random() * (151 - 2) + 2)
+const response = await fetch(`${baseURL}${randNum}`);
+console.log(response);
+const json = await response.json();
+displayPokemon(json)
+console.log(json)
