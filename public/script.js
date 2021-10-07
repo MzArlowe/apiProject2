fcec2826997d38e13d121b9e2098d157cdfe69dc
unknown <bhm727@yahoo.com>
2021-10-07 17:43:42 -0400

    Small udpates

--- public/script.js
+++ public/script.js
@@ -7,43 +7,44 @@ let btn = document.querySelector("button");
 
 btn.addEventListener("click", (fetchResults));
 
-// fetch(url)
-// .then(function(picInfo){
-//     return picInfo.json();
-// })
-// .then(function(json){
-//     displayResults(json);
-//     btn.innerText = "Click to see more!";
-// })
+fetch(url)
+    .then(function (picInfo) {
+        return picInfo.json();
+    })
+    .then(function (json) {
+        displayResults(json);
+        btn.innerText = "Click to see more!";
+    })
 
-// const randNum = Math.floor(Math.random() * (151 - 2) + 2)
-// const response = await fetch(`${baseURL}${randNum}`);
 function fetchResults() {
     fetch(url)
-    .then(function(result){
-        return result.json();
-    }).then(function(json) {
-        displayResults(json);
-    })
-    console.log(json);
+        .then(function (result) {
+            return result.json();
+        }).then(function (json) {
+            displayResults(json);
+        })
+    // console.log(json);  //if you log this in the console, when you click the button, you will see the objects and arrays pop up!
+}
 function displayResults(json) {
     console.log(json);
-    let newImage = document.createElement('img');
-    newImage.src = json.url;
+
+    let img = document.getElementById("imageBox")
+    let dtl = document.getElementById("descriptionBox")
+    let newImage = document.createElement('img');  //this creates the element needed to push image into it.
+
+    newImage.src = json[0].url;  //this actually sends the img to the element. If you look in the console at the json, you will see how the object is numbered [0].
+
+    img.appendChild(newImage); //This is the only thing I added in your html. <div id="imagePlease"></div> This is what appends the existing element in HTML and the new one created.
+    
+    while (randomDate.firstChild) {
+        newImage.removeChild(img)
+    }
+
+    picTitle.innerText = `${json[0].title}`;
+    picImage.src = json[0].hdurl;
+    picImage.alt = "Astronomy image";
+    picExplanation.innerText = `${json[0].explanation}`;
+
+    description_box.appendChild(dtl);
+
 }
-
-// let img = document.getElementById("image_box")
-// let dtl = document.getElementById("description_box")
-
-// while(randomDate.firstChild) {
-//     randomDate.removeChild
-// }
-
-// picTitle.innerText = `${json[0].title}`;
-// picImage.src = json[0].hdurl;
-// picImage.alt = "Astronomy image";
-// picExplanation.innerText = `${json[0].explanation}`;
-
-// description_box.appendChild(dtl);
-
-// }}
